c3ea7b2ae818927ccc6963b6ed0d70e22c6f6e1c
MahnoorRehman <mahnoorrehman061@gmail.com>
2023-01-17 22:51:16 +0000

    Make a responsive header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding ' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the New Value" />
+      <h1 className="app__header-h1">Key to Find Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        It is a form of dummy text
+        that has been used in the typesetting industry for centuries. The text itself is
+        nonsensical and consists of a random mix of Latin words.
+      </p>
+      <button className="custom__button">Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="Header Img" />
+    </div>
   </div>
 );
 
