abccc1b800071cd8f78cfec4f7e18368e18a382c
MahnoorRehman <mahnoorrehman061@gmail.com>
2023-01-18 22:25:02 +0000

    Add Special Menu in website and some other features

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
 import './Chef.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="image_chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Words" />
+      <h1 className="headtext__cormorant">What we Believe In</h1>
+      <div className="app__chef-content">\
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="" />
+          <p className='p__opensans'>
+            It is a form of dummy text that has been used in</p>
+        </div>
+        <p className='p__opensans'>
+          the typesetting industry for centuries.
+          The text itself is nonsensical and consists of a random mix of Latin words.
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="" />
+      </div>
+    </div>
+  </div >
 );
 
 export default Chef;
